b081b6a16c2f9b326cb1df84cf15b1fa9b4a5c14
Bartłomiej Karwowski <bkarw12@gmail.com>
2021-10-15 22:34:37 +0200

    Get board size and print the state

--- src/main/java/karwowski/game_of_life/Main.java
+++ src/main/java/karwowski/game_of_life/Main.java
@@ -1,7 +1,28 @@
 package karwowski.game_of_life;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int m = 0, n = 0;
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter board width: ");
+            m = sc.nextInt();
+            System.out.print("Enter board height: ");
+            n = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Wrong input!");
+            System.exit(1);
+        }
+
+        if (m <= 0 || n <= 0) {
+            System.out.println("Input numbers should be positive!");
+            System.exit(1);
+        }
+
+        Board board = Board.randomBoard(m, n);
+        System.out.println(board);
     }
 }
