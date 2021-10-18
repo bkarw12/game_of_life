f123fc64930db1d5916867f757f2dee85b8f7ac5
Bartłomiej Karwowski <bkarw12@gmail.com>
2021-10-18 13:54:26 +0200

    Finish basic rendering function

--- src/main/java/karwowski/game_of_life/BoardRenderer.java
+++ src/main/java/karwowski/game_of_life/BoardRenderer.java
@@ -1,7 +1,19 @@
 package karwowski.game_of_life;
 
 public class BoardRenderer {
-    public static void render(Board board) {
+    private static final char EMPTY_CELL = ' ';
+    private static final char FULL_CELL = '#';
 
+    public static void render(Board board) {
+        System.out.println("-".repeat(board.getWidth() + 2));
+        for (int j = 0; j < board.getHeight(); j++) {
+            System.out.print("|");
+            for (int i = 0; i < board.getWidth(); i++) {
+                System.out.print((board.getState()[i][j] == 1) ?
+                        FULL_CELL : EMPTY_CELL);
+            }
+            System.out.println("|");
+        }
+        System.out.println("-".repeat(board.getWidth() + 2));
     }
 }
