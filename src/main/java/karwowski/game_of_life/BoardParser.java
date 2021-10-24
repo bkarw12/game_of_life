76ce24087c08c1561b92255c9518e402157227db
Bartłomiej Karwowski <bkarw12@gmail.com>
2021-10-24 10:43:48 +0200

    Add BoardParserTest

--- src/main/java/karwowski/game_of_life/BoardParser.java
+++ src/main/java/karwowski/game_of_life/BoardParser.java
@@ -7,6 +7,11 @@ import java.util.*;
  * BoardParser is responsible for parsing Board state data from input files.
  */
 public class BoardParser {
+    public static final String WRONG_CHARACTER_INPUT =
+            "Input file can only contain 0's and 1's!";
+    public static final String WRONG_ROW_LENGTH_INPUT =
+            "Some input file line lengths aren't identical!";
+
     public static Board parse(BufferedReader reader) throws IOException {
         int m = 0, n = 0;
         var cells = new LinkedList<Integer>();
@@ -19,9 +24,9 @@ public class BoardParser {
         while (line != null) {
             m++;
             if (!line.matches("[01]+"))
-                throw new IOException("Input file can only contain 0's and 1's!");
+                throw new IOException(WRONG_CHARACTER_INPUT);
             if (line.length() != n)
-                throw new IOException("Some input file line lengths aren't identical!");
+                throw new IOException(WRONG_ROW_LENGTH_INPUT);
 
             // Line is okay, parse it
             for (char c: line.toCharArray()) {
--- src/test/java/karwowski/game_of_life/BoardParserTest.java
+++ src/test/java/karwowski/game_of_life/BoardParserTest.java
@@ -1,2 +1,60 @@
-package karwowski.game_of_life;public class BoardParserTest {
+package karwowski.game_of_life;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+import java.io.*;
+
+public class BoardParserTest {
+    private static final String INPUT_PATH = "input/";
+
+    BufferedReader reader;
+    Board board;
+
+    @Test
+    @DisplayName("Good input gets parsed and accepted")
+    void testGoodInput() {
+        try {
+            byte[][] expectedState = {
+                    {0, 0, 0, 0, 0},
+                    {0, 0, 1, 0, 0},
+                    {0, 0, 1, 0, 0},
+                    {0, 0, 1, 0, 0},
+                    {0, 0, 0, 0, 0}
+            };
+
+            reader = new BufferedReader(new FileReader(INPUT_PATH + "good.txt"));
+            board = BoardParser.parse(reader);
+            assertArrayEquals(board.getState(), expectedState);
+        } catch (Exception e) {
+            fail("Test failed: " + e.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Bad input with other characters than 0's/1's throws an exception")
+    void testBadInputText() {
+        try {
+            reader = new BufferedReader(new FileReader(INPUT_PATH + "bad.txt"));
+            board = BoardParser.parse(reader);
+            fail("Test failed: expected IOException: "
+                    + BoardParser.WRONG_CHARACTER_INPUT);
+        } catch (IOException e) {
+            assertEquals(e.getMessage(), BoardParser.WRONG_CHARACTER_INPUT);
+        }
+    }
+
+    @Test
+    @DisplayName("Bad input with wrong uneven row lenghts throws an exception")
+    void testBadInputRowLength() {
+        try {
+            reader = new BufferedReader(new FileReader(INPUT_PATH + "bad2.txt"));
+            board = BoardParser.parse(reader);
+            fail("Test failed: expected IOException: "
+                    + BoardParser.WRONG_ROW_LENGTH_INPUT);
+        } catch (IOException e) {
+            assertEquals(e.getMessage(), BoardParser.WRONG_ROW_LENGTH_INPUT);
+        }
+    }
 }
